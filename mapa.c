6912d8aa2c8604552c7d8df9697e4a5289c0ec42
Edws <eduardos.rocha17@gmail.com>
2020-10-29 12:27:55 -0300

    Corrigindo mapa e removendo jogos onde uma vitória já foi alcançada

--- mapa.c
+++ mapa.c
@@ -17,8 +17,18 @@ static int simetrias[8][9] = {{0, 1, 2, 3, 4, 5, 6, 7, 8},
                               {6, 3, 0, 7, 4, 1, 8, 5, 2},
                               {0, 3, 6, 1, 4, 7, 2, 5, 8}};
 
+static int vitorias[8][3] = {{0, 1, 2},
+                             {3, 4, 5},
+                             {6, 7, 8},
+                             {0, 3, 6},
+                             {1, 4, 7},
+                             {2, 5, 8},
+                             {0, 4, 8},
+                             {2, 4, 6}};
+
 inline static int min(int a, int b) { return (a < b ? a : b); }
 inline static int max(int a, int b) { return (a > b ? a : b); }
+
 /**
  * Retorna x elevado a
  */
@@ -48,6 +58,18 @@ static int calc_min(int* jogo) {
     return minimo;
 }
 
+static bool venceu(int* jogo) {
+    int i;
+    int a, b, c;
+    for (i = 0; i < 8; i++) {
+        a = vitorias[i][0];
+        b = vitorias[i][1];
+        c = vitorias[i][2];
+        if (jogo[a] != 0 && jogo[a] == jogo[b] && jogo[b] == jogo[c]) return true;
+    }
+    return false;
+}
+
 /**
  * jogos deve comecar com o primeiro jogo com tudo 0
  * n deve comecar valendo 1 e sera usado para armazenar dados no vetor
@@ -68,6 +90,7 @@ static bool _jogos_possiveis(int** jogos, int* n, int atual, int vez) {
 
         // Faz jogada
         jogos[k][i] = vez;
+        if (venceu(jogos[k])) continue;
 
         // Passa para a funcao fazer outra jogada
         ok = _jogos_possiveis(jogos, n, k, (vez == 1 ? 2 : 1));
@@ -124,39 +147,37 @@ static void salva_mapa(int* mapa, int n, int m) {
 int main() {
     int i;
 
-    int n;
-    int** jogos = jogos_possiveis(&n);
-    int minimos[M], minimo_para_gene[M];
-    for (i = 0; i < M; i++) minimo_para_gene[i] = minimos[i] = -1;
+    int m;
+    int** jogos = jogos_possiveis(&m);
+    int minimos[M];  // 0..M -> 0..N
+    for (i = 0; i < M; i++) minimos[i] = -1;
     bool freq[N];
     for (i = 0; i < N; i++) freq[i] = false;
-    int mapa[N];
+    int mapa[N];  // 0..N -> 0..tam_cromossomo
     for (i = 0; i < N; i++) mapa[i] = -1;
     int tam_mapa = -1;       // Maior valor entre os minimos
     int tam_cromossomo = 0;  // Numero de valores unicos em minimos
 
     if (!jogos) goto falha;
 
-    for (i = 0; i < M; i++) {
-        minimos[i] = calc_min(jogos[i]);
+    // Calcula os minimos de cada jogo
+    for (i = 0; i < m; i++) minimos[i] = calc_min(jogos[i]);
+
+    for (i = 0; i < m; i++) {
         tam_mapa = max(minimos[i], tam_mapa);
         freq[minimos[i]] = true;
     }
-    tam_mapa++;  // Tem que incluir o maior também
+    tam_mapa++;  // Tem que incluir o maior tambem
     for (i = 0; i < N; i++) tam_cromossomo += freq[i];
-
-    int acc = 0;
-    for (i = 0; i < M; i++) {
-        if (freq[i]) {
-            minimo_para_gene[i] = acc++;
-        }
-    }
-
     printf("Tam mapa: %d; Tam cromossomo %d\n", tam_mapa, tam_cromossomo);
 
-    for (i = 0; i < M; i++) {
-        mapa[minimos[i]] = minimo_para_gene[minimos[i]];
-        printf("%d -> %d\n", minimos[i], minimo_para_gene[minimos[i]]);
+    int acc = 0;
+    for (i = 0; i < N; i++) {
+        if (freq[i]) {
+            mapa[i] = acc;
+            acc += 1;
+        }
+        printf("%d -> %d\n", i, mapa[minimos[i]]);
     }
 
     salva_mapa(mapa, tam_mapa, tam_cromossomo);
