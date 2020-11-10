04f6802f8bd5343fa96d96dbe4a4a6264c2994cd
Edws <eduardos.rocha17@gmail.com>
2020-11-10 20:49:50 -0300

    Refatorando mapa
    # Em gerar_mapa:
    Transformando vetor simetria em um comentário já que esse trecho de código não é usado
    Adicionando comentários
    Refatorando o código
    Mudando o que é impressos no stdout, mostrando alguns detalhes sobre o mapa gerando e mostrando que o algoritmo acabou

--- mapa/main.c
+++ mapa/main.c
@@ -7,14 +7,14 @@
 const int N = 19683;   // powi(3, 9); numero de arranjos possiveis de X e O
 const int M = 986410;  //409114;  // 1 + 9 + 9*8 + 9*8*7 + ... + 9*8*7*6*5*4*3*2*1 ; Numemro maximo de recursoes de jogo_possiveis()
 
-static int simetrias[8][9] = {{0, 1, 2, 3, 4, 5, 6, 7, 8},
-                              {2, 1, 0, 5, 4, 3, 8, 7, 6},
-                              {2, 5, 8, 1, 4, 7, 0, 3, 6},
-                              {8, 5, 2, 7, 4, 1, 6, 3, 0},
-                              {8, 7, 6, 5, 4, 3, 2, 1, 0},
-                              {6, 7, 8, 3, 4, 5, 0, 1, 2},
-                              {6, 3, 0, 7, 4, 1, 8, 5, 2},
-                              {0, 3, 6, 1, 4, 7, 2, 5, 8}};
+// static int simetrias[8][9] = {{0, 1, 2, 3, 4, 5, 6, 7, 8},
+//                               {2, 1, 0, 5, 4, 3, 8, 7, 6},
+//                               {2, 5, 8, 1, 4, 7, 0, 3, 6},
+//                               {8, 5, 2, 7, 4, 1, 6, 3, 0},
+//                               {8, 7, 6, 5, 4, 3, 2, 1, 0},
+//                               {6, 7, 8, 3, 4, 5, 0, 1, 2},
+//                               {6, 3, 0, 7, 4, 1, 8, 5, 2},
+//                               {0, 3, 6, 1, 4, 7, 2, 5, 8}};
 
 static int simetrias_reversa[8][9] = {{0, 1, 2, 3, 4, 5, 6, 7, 8},
                                       {2, 1, 0, 5, 4, 3, 8, 7, 6},
@@ -25,6 +25,9 @@ static int simetrias_reversa[8][9] = {{0, 1, 2, 3, 4, 5, 6, 7, 8},
                                       {2, 5, 8, 1, 4, 7, 0, 3, 6},
                                       {0, 3, 6, 1, 4, 7, 2, 5, 8}};
 
+/**
+ * Vetor com todas as opcoes de vitoria
+ */
 static int vitorias[8][3] = {{0, 1, 2},
                              {3, 4, 5},
                              {6, 7, 8},
@@ -66,6 +69,9 @@ static int calc_min(int* jogo) {
     return minimo;
 }
 
+/**
+ * Retorna se alguém venceu o jogo
+ */
 static bool venceu(int* jogo) {
     int i;
     int a, b, c;
@@ -78,6 +84,9 @@ static bool venceu(int* jogo) {
     return false;
 }
 
+/**
+ * Retorna se o jogo deu velha
+ */
 static bool cheio(int* jogo) {
     int i;
     bool zero = false;
@@ -129,8 +138,9 @@ falha:
  * Retorna em n quantas posicoes foram usadas
  */
 static inline int** jogos_possiveis(int* n) {
-    int i;
     int** jogos = NULL;
+
+    int i;
     bool ok;
 
     // Aloca array de jogos
@@ -158,30 +168,41 @@ falha:
     return NULL;
 }
 
-static void salva_mapa(int* mapa, int n, int m) {
+/**
+ * Salva mapa em uma arquivo em path
+ */
+static void salva_mapa(char* path, int* mapa, int tam_mapa, int tam_cromossomo) {
     int i;
-    FILE* arquivo = fopen("mapa.txt", "w");
+    FILE* arquivo = fopen(path, "w");
     if (!arquivo) return;
 
-    fprintf(arquivo, "%d %d\n", n, m);
-    for (i = 0; i < n; i++) fprintf(arquivo, "%d\n", mapa[i]);
+    fprintf(arquivo, "%d %d\n", tam_mapa, tam_cromossomo);
+    for (i = 0; i < tam_mapa; i++) fprintf(arquivo, "%d\n", mapa[i]);
     fclose(arquivo);
 }
 
+/**
+ * Gera um mapa e salva em mapa.txt
+ * Esse mapa serve para mapear todos os possivei jogos
+ * eliminando jogos simetricos e fazendo com que o vetor
+ * com todas as jogadas a ser feita seja menor
+ */
 int main() {
-    int i;
+    int** jogos = NULL;
 
+    int i;
     int m;
-    int** jogos = jogos_possiveis(&m);
-    int minimos[M];  // 0..M -> 0..N
-    for (i = 0; i < M; i++) minimos[i] = -1;
-    bool freq[N];
-    for (i = 0; i < N; i++) freq[i] = false;
-    int mapa[N];  // 0..N -> 0..tam_cromossomo
-    for (i = 0; i < N; i++) mapa[i] = -1;
+    int minimos[M];          // 0..M -> 0..N
+    bool freq[N];            //
+    int mapa[N];             // 0..N -> 0..tam_cromossomo
     int tam_mapa = -1;       // Maior valor entre os minimos
     int tam_cromossomo = 0;  // Numero de valores unicos em minimos
 
+    for (i = 0; i < M; i++) minimos[i] = -1;
+    for (i = 0; i < N; i++) freq[i] = false;
+    for (i = 0; i < N; i++) mapa[i] = -1;
+
+    jogos = jogos_possiveis(&m);
     if (!jogos) goto falha;
 
     // Calcula os minimos de cada jogo
@@ -193,21 +214,26 @@ int main() {
     }
     tam_mapa++;  // Tem que incluir o maior tambem
     for (i = 0; i < N; i++) tam_cromossomo += freq[i];
-    printf("Tam mapa: %d; Tam cromossomo %d\n", tam_mapa, tam_cromossomo);
 
+    // Calcula mapa
     int acc = 0;
     for (i = 0; i < N; i++) {
         if (freq[i]) {
             mapa[i] = acc;
             acc += 1;
         }
-        printf("%d -> %d\n", i, mapa[minimos[i]]);
     }
 
-    salva_mapa(mapa, tam_mapa, tam_cromossomo);
+    // Salva o mapa e um arquivo
+    salva_mapa("mapa.txt", mapa, tam_mapa, tam_cromossomo);
 
+    // Desalocando memória
     for (i = 0; i < M; i++) free(jogos[i]);
     free(jogos);
+
+    printf("Mapa salvo em mapa.txt\n");
+    printf("Tamanho do mapa: %d\n", tam_mapa);
+    printf("Tamanho do cromossomo: %d\n", tam_cromossomo);
     return EXIT_SUCCESS;
 
 falha:
@@ -215,6 +241,6 @@ falha:
         for (i = 0; i < M; i++) free(jogos[i]);
         free(jogos);
     }
-    printf("Mapeamento falhou\n");
+    printf("Falha ao criar mapa\n");
     return EXIT_FAILURE;
 }
