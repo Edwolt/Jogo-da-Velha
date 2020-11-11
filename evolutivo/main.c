ade8f628b315b1a423afa7769ec77b92fa5bba7b
Edws <eduardos.rocha17@gmail.com>
2020-11-11 11:50:11 -0300

    Criando população
    Criando função para criar a população

--- evolutivo/main.c
+++ evolutivo/main.c
@@ -1,21 +1,60 @@
 #include <stdlib.h>  // malloc, rand
 #include <time.h>
+#include "kbhit.h"
 #include "solucao.h"
 
-Solucao* random_solucao() {
-    Solucao* sol = solucao_novo();
-    if (!sol) return NULL;
-}
-
-int* gerar_populacao(int n) {
+Solucao** populacao_criar(int n) {
+    Solucao** populacao = NULL;
     int i;
-    Solucao** populacao = malloc(n * sizeof(Solucao*));
+
+    populacao = malloc(n * sizeof(Solucao*));
+    if (!populacao) goto falha;
+    for (i = 0; i < n; i++) populacao[i] = NULL;
 
     for (i = 0; i < n; i++) {
-        populacao = random_solucao();
+        populacao[i] = solucao_criar_random();
+        if (!populacao[i]) goto falha;
     }
+
+    return populacao;
+
+falha:
+    if (populacao) {
+        for (i = 0; i < n; i++) solucao_apagar(&populacao[i]);
+        free(populacao);
+    }
+    return NULL;
+}
+
+void populacao_apagar(Solucao*** populacao, int n) {
+    if (!populacao || !*populacao) return;
+
+    int i;
+    for (i = 0; i < n; i++) solucao_apagar(&(*populacao)[i]);
+    free(*populacao);
+    *populacao = NULL;
 }
 
 int main() {
     srand(time(NULL));
+
+    Solucao** populacao = NULL;
+    const int N = 500;
+
+    populacao = populacao_criar(N);
+    if (!populacao) goto falha;
+
+    enable_raw_mode();
+    while (true) {
+        if (kbhit()) break;
+    }
+    disable_raw_mode();
+
+    populacao_apagar(&populacao, N);
+    return EXIT_SUCCESS;
+
+falha:
+    populacao_apagar(&populacao, N);
+    printf("Falha ao rodar algoritmo evolutivo\n");
+    return EXIT_FAILURE;
 }
