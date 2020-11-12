d641624cd5e9446f1e2ce552f59c7dfa24e829ab
Edws <eduardos.rocha17@gmail.com>
2020-11-12 16:21:19 -0300

    Continuando desenvolvimento do algoritmo evolutivo (nao testado)

--- evolutivo/main.c
+++ evolutivo/main.c
@@ -3,7 +3,17 @@
 #include "kbhit.h"
 #include "solucao.h"
 
-Solucao** populacao_criar(int n) {
+extern int tam_cromossomo;
+const int N = 500;
+
+inline static randbool() { return rand() % 2; }
+inline static void swap(Solucao** a, Solucao** b) {
+    Solucao* aux = *a;
+    *a = *b;
+    *b = aux;
+}
+
+static Solucao** _populacao_criar(int n, Solucao* criar()) {
     Solucao** populacao = NULL;
     int i;
 
@@ -12,21 +22,21 @@ Solucao** populacao_criar(int n) {
     for (i = 0; i < n; i++) populacao[i] = NULL;
 
     for (i = 0; i < n; i++) {
-        populacao[i] = solucao_criar_random();
+        populacao[i] = criar();
         if (!populacao[i]) goto falha;
     }
 
     return populacao;
 
 falha:
-    if (populacao) {
-        for (i = 0; i < n; i++) solucao_apagar(&populacao[i]);
-        free(populacao);
-    }
+    populacao_apagar(&populacao, n);
     return NULL;
 }
 
-void populacao_apagar(Solucao*** populacao, int n) {
+inline static Solucao** populacao_criar_vazio(int n) { _populacao_criar(n, solucao_criar); }
+inline static Solucao** populacao_criar(int n) { _populacao_criar(n, solucao_criar_random); }
+
+static void populacao_apagar(Solucao*** populacao, int n) {
     if (!populacao || !*populacao) return;
 
     int i;
@@ -35,17 +45,87 @@ void populacao_apagar(Solucao*** populacao, int n) {
     *populacao = NULL;
 }
 
+static void crossover(Solucao* a, Solucao* b) {
+    int i;
+    for (i = 0; i < tam_cromossomo; i++) {
+        if (randbool()) solucao_gene_set(a, i, solucao_gene_get(b, i));
+    }
+}
+
+static bool populacao_torneio(Solucao* a, Solucao* b, int n) {
+    int* nova_populacao = NULL;
+
+    nova_populacao = populacao_criar_vazio(n);
+
+    if (!nova_populacao) goto falha;
+
+falha:
+    populacao_apagar(nova_populacao, n);
+    return false;
+}
+
+static int jogar(Solucao* a, Solucao* b) {
+    int jogo[9];
+    int i;
+    int vez = 1;
+    int vencedor;
+    int pontuacao = 0;
+
+    for (i = 0; i < 9; i++) jogo[i] = 0;
+
+    do {
+        if (vez == 1) {
+            jogo[solucao_get(a, jogo)] = vez;
+        } else {
+            jogo[solucao_get(b, jogo)] = vez;
+        }
+        vez = trocar_vez(vez);
+    } while (vencedor == 1 || vencedor == 2);
+
+    vencedor = calc_vencedor(jogo);
+    if (vencedor == 1) {
+        pontuacao++;
+    } else if (vencedor == 2) {
+        pontuacao--;
+    }
+
+    return pontuacao;
+}
+
+int encontrar_maior(int* fitness, int n) {
+    int i;
+    int maior = 0;
+    for (i = 1; i < n; i++) {
+        if (fitness[i] > fitness[maior]) maior = i;
+    }
+    return maior;
+}
+
 int main() {
     srand(time(NULL));
 
     Solucao** populacao = NULL;
-    const int N = 500;
+    int i, j, k;
+    int fitness[N];
+
+    for (i = 0; i < N; i++) fitness[i] = 0;
 
     populacao = populacao_criar(N);
     if (!populacao) goto falha;
 
     enable_raw_mode();
     while (true) {
+        for (i = 0; i < N; i++) {
+            for (j = 0; j < N; j++) {
+                fitness[i] += jogar(populacao[i], populacao[j]);
+            }
+        }
+
+        k = encontrar_maior(fitness, N);
+        swap(&populacao[0], &populacao[k]);
+
+        // crossover_todos(a, b);
+
         if (kbhit()) break;
     }
     disable_raw_mode();
