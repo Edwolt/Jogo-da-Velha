cabdc595d7f1ad2b278f5a132d33c3b6c140a58c
Eduardo Souza Rocha <eduardos.rocha17@gmail.com>
2021-05-27 23:11:06 -0300

    Rust: Algumas mudanças na struct jogo

--- versao_rust/src/jogo.rs
+++ versao_rust/src/jogo.rs
@@ -49,7 +49,7 @@ pub const POW3: [usize; 10] = [
 
 /// Um valor no tabuleiro, a vez do Jogador ou o resultado do jogo
 #[derive(Eq, PartialEq, Clone, Copy)]
-enum Vez {
+pub enum Vez {
     /// Tabuleiro: Espaço vazio
     /// Vez:       -
     /// Resultado: Jogo não terminou
@@ -72,7 +72,7 @@ enum Vez {
 }
 
 impl Vez {
-    const fn from_num(val: usize) -> Vez {
+    pub const fn from_num(val: usize) -> Vez {
         match val {
             0 => Z,
             1 => X,
@@ -83,7 +83,7 @@ impl Vez {
     }
 
     /// Muda a jogada entre X e O
-    const fn trocar(self) -> Vez {
+    pub const fn trocar(self) -> Vez {
         match self {
             Z => Z,
             X => O,
@@ -92,25 +92,25 @@ impl Vez {
         }
     }
 
-    const fn venceu(self) -> bool {
+    pub const fn venceu(self) -> bool {
         match self {
             X | O => true,
             _ => false,
         }
     }
 
-    const fn velha(self) -> bool {
+    pub const fn velha(self) -> bool {
         matches!(self, V)
     }
 
-    const fn venceu_ou_velha(self) -> bool {
+    pub const fn venceu_ou_velha(self) -> bool {
         match self {
             X | O | V => true,
             _ => false,
         }
     }
 
-    const fn num(self) -> usize {
+    pub const fn num(self) -> usize {
         match self {
             Z => 0,
             X => 1,
@@ -121,16 +121,20 @@ impl Vez {
 }
 
 #[derive(Clone)]
-struct Jogo {
-    tab: [Vez; 9],
+pub struct Jogo {
+    jogo: [Vez; 9],
 }
 
 impl Jogo {
-    fn new() -> Jogo {
-        Jogo { tab: [Z; 9] }
+    pub const fn criar() -> Jogo {
+        Jogo { jogo: [Z; 9] }
     }
 
-    fn numero(&self) -> usize {
+    pub const fn crair_com(jogo: [Vez; 9]) -> Jogo {
+        Jogo { jogo }
+    }
+
+    pub fn numero(&self) -> usize {
         let mut numero = 0;
         for i in 0..self.len() {
             numero += self[i].num() * POW3[i];
@@ -138,7 +142,7 @@ impl Jogo {
         numero
     }
 
-    fn minimo(&self) -> usize {
+    pub fn minimo(&self) -> usize {
         let mut minimo = usize::MAX;
         for i in SIMETRIAS_REVERSA.iter() {
             let mut numero = 0;
@@ -150,7 +154,7 @@ impl Jogo {
         minimo
     }
 
-    fn simetria(&self) -> usize {
+    pub fn simetria(&self) -> usize {
         let mut minimo = usize::MAX;
         let mut simetria = 0;
 
@@ -169,7 +173,7 @@ impl Jogo {
         simetria
     }
 
-    fn valor(&self, simetria: usize) -> usize {
+    pub fn valor(&self, simetria: usize) -> usize {
         let mut numero = 0;
         for i in 0..self.len() {
             numero += SIMETRIAS_REVERSA[simetria][i] * POW3[i];
@@ -177,14 +181,14 @@ impl Jogo {
         numero
     }
 
-    fn resultado(&self) -> Vez {
+    pub fn resultado(&self) -> Vez {
         for &(a, b, c) in VITORIAS.iter() {
             if self[a] == Z && self[a] == self[b] && self[b] == self[c] {
                 return self[a];
             }
         }
 
-        for &i in self.tab.iter() {
+        for &i in self.jogo.iter() {
             if i == Z {
                 return Z;
             }
@@ -193,7 +197,7 @@ impl Jogo {
         V
     }
 
-    fn possibilidades() -> Vec<Jogo> {
+    pub fn possibilidades() -> Vec<Jogo> {
         fn rec_possibilidades(jogos: &mut Vec<Jogo>, vez: Vez) {
             for i in 0..9 {
                 let atual = jogos.last().unwrap();
@@ -210,12 +214,12 @@ impl Jogo {
             }
         }
 
-        let mut jogos: Vec<Jogo> = vec![Jogo::new()];
+        let mut jogos: Vec<Jogo> = vec![Jogo::criar()];
         rec_possibilidades(&mut jogos, X);
         jogos
     }
 
-    const fn len(&self) -> usize {
+    pub const fn len(&self) -> usize {
         9
     }
 }
@@ -223,12 +227,12 @@ impl Jogo {
 impl Index<usize> for Jogo {
     type Output = Vez;
     fn index(&self, i: usize) -> &Vez {
-        &self.tab[i]
+        &self.jogo[i]
     }
 }
 
 impl IndexMut<usize> for Jogo {
     fn index_mut(&mut self, i: usize) -> &mut Vez {
-        &mut self.tab[i]
+        &mut self.jogo[i]
     }
 }
